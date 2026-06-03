aec05fdda8657e93d0f6c0f5af8e979fdfea440a
shranav-1148 <shranavsakha78@gmail.com>
2026-06-02 20:35:18 -0500

    Adding two more api routes

--- server/app/main.py
+++ server/app/main.py
@@ -4,6 +4,8 @@ from app.api.health import router as health_router
 from app.api.repositories import router as repository_router
 from app.api.chat import router as chat_router
 from app.api.auth import router as auth_router
+from app.api.messages import router as messages_router
+from app.api.users import router as users_router
 
 app = FastAPI()
 
@@ -11,3 +13,5 @@ app.include_router(health_router)
 app.include_router(repository_router)
 app.include_router(chat_router)
 app.include_router(auth_router)
+app.include_router(messages_router)
+app.include_router(users_router)
