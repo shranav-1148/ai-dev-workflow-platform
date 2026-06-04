6a711126bdc7a0192fb50ed1ac45e787cc422421
shranav-1148 <shranavsakha78@gmail.com>
2026-06-04 16:38:32 -0500

    feature: completing GET requests for repositories

--- server/app/api/repositories.py
+++ server/app/api/repositories.py
@@ -1,26 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.repositories import RepositoryCreate, RepositoryResponse
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.models.repositories import Repository
 
 
 router = APIRouter(prefix="/repositories")
 
-@router.get("/")
-def get_repositories():
-    return [
-        RepositoryResponse(id=1, name="repo-1", url="https://example.com/repo-1"),
-        RepositoryResponse(id=2, name="repo-2", url="https://example.com/repo-2"),
-    ]
+@router.get("/", response_model=list[RepositoryResponse])
+def get_repositories(db : Session = Depends(get_db)):
+    repositories = db.query(Repository).all()
 
-@router.get("/{repo_id}")
-def get_repository(repo_id: int):
-    return RepositoryResponse(id=repo_id, name=f"repo-{repo_id}", url=f"https://example.com/repo-{repo_id}")
+    return repositories
+
+@router.get("/{repo_id}", response_model=RepositoryResponse)
+def get_repository(repo_id: int, db : Session = Depends(get_db)):
+    repository = db.query(Repository).filter(Repository.id == repo_id).first()
+    
+    return repository
 
 
 @router.post("/", response_model=RepositoryResponse)
-def create_repository(repo: RepositoryCreate):
-
-    return RepositoryResponse(
-        id = 1,
+def create_repository(repo: RepositoryCreate, db:Session = Depends(get_db)):
+    db_repo = Repository(
         name=repo.name,
         github_url=repo.github_url
     )
+
+    db.add(db_repo)
+    db.commit()
+    db.refresh(db_repo)
+
+    return db_repo
+
+
+
+    
--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, SessionLocal
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
