a4909709f8aee11fcff7d8f56c2c679b3cb57c8a
shranav-1148 <shranavskha78@gmail.com>
2026-07-02 15:20:32 -0500

    FIX: Template resolver upgrade to include lists and dictionaries

--- server/app/services/template_resolver.py
+++ server/app/services/template_resolver.py
@@ -3,6 +3,20 @@ import re
 def resolve_templates(value, context):
     '''Resolves output from previous steps that were part of a current step configuration and returns
     the value from a string. Used in determining whether the current step can be executed'''
+
+    if isinstance(value, dict):
+        return {
+            key: resolve_templates(val, context)
+            for key, val in value.items()
+        }
+    
+    if isinstance(value, list):
+        return [
+            resolve_templates(item, context)
+            for item in value
+        ]
+
+
     if not isinstance(value, str):
         return value
     
